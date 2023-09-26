d8470d2cdf38cd717a466ddabcd963f4ed12ccd1
Karim EL RHOURHA <karim.elrhourha@gmail.com>
2023-09-26 10:37:16 +0200

    upgrade version

---
+++ admin/webpack.config.js
@@ -0,0 +1,11 @@
+'use strict';
+// WARNING: the admin panel now uses webpack 5 to bundle the application.
+module.exports = (config, webpack) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack configuration
+    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
+    config.resolve.alias['@strapi/icons'] = path.resolve('node_modules', '@strapi/icons/dist');
+
+    // Important: return the modified configuration
+    return config;
+};

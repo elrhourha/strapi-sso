1796b9e339f1eae33750f202b6d542ff23dd39c6
Karim EL RHOURHA <karim.elrhourha@gmail.com>
2023-09-27 11:06:40 +0200

    add oauth service

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const bootstrap = require('./bootstrap');
 const destroy = require('./destroy');
 const config = require('./config');
 const controllers = require('./controllers');
+const services = require('./services');
 const routes = require('./routes');
 const policies = require('./policies');
 

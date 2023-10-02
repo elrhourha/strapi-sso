bbbe2efd89e8b091461c4289a2e6b2d30c8c4389
Karim EL RHOURHA <karim.elrhourha@gmail.com>
2023-10-02 08:54:03 +0200

    wip

--- server/config/index.js
+++ server/config/index.js
@@ -2,7 +2,6 @@
 
 module.exports = {
   default: {
-    GITHUB_OAUTH_REDIRECT_URI: 'http://localhost:1337/strapi-plugin-strapi-sso/github/callback',
     GOOGLE_GSUITE_HD: '',
     GOOGLE_ALIAS: '',
   },
